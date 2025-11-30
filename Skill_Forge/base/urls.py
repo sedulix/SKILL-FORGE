bd650cfb716790091e29c4d009d0c08bbe8f1ad7
Максим Ким <st0cktonbl4@gmail.com>
2025-11-30 23:10:04 +0500

    v 0.5 mk8

--- Skill_Forge/base/urls.py
+++ Skill_Forge/base/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('landing.urls')),
+    path('skillforge/', include('landing.urls')),
 ]
 
 
--- Skill_Forge/landing/urls.py
+++ Skill_Forge/landing/urls.py
@@ -4,7 +4,7 @@ from . views import index, TestPageView, OurTeamView, About
 app_name = 'landing'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('landing/', index, name='index'),
     path('test/', TestPageView.as_view(), name='test'),
     path('ourteam/', OurTeamView.as_view(), name='ourteam'),
     path('about/', About.as_view(), name='about')
