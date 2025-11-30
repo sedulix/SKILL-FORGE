1005fc9244e0cce40cdb3bd8b9c00222e2422c3c
Максим Ким <st0cktonbl4@gmail.com>
2025-11-30 16:52:41 +0500

    v 0.3 mk8

--- Skill_Forge/landing/urls.py
+++ Skill_Forge/landing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . views import index
+from . views import index, TestPageView
 
+app_name = 'landing'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('test/', TestPageView.as_view(), name='test'),
 ]
 
 
--- Skill_Forge/landing/views.py
+++ Skill_Forge/landing/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
 
 def index(request):
     return render(request, 'landing/index.html')
+
+class TestPageView(View):
+    def get(self, request):
+        context = {
+
+        }
+        return render(request, 'landing/test.html', context)
 
 
 
