4ed5a574beea98afd09d647954400d879ba965fd
Максим Ким <st0cktonbl4@gmail.com>
2025-11-30 23:21:30 +0500

    v 0.5 mk8

--- Skill_Forge/base/urls.py
+++ Skill_Forge/base/urls.py
@@ -21,7 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('skillforge/', include('landing.urls')),
+    path('', include('landing.urls')),
 ]
-
-
